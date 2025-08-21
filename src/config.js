f676b0bc29cfee3c52547711e440b5e3bc804646
Bobhate Vishal <bobhatev13@gmail.com>
2025-08-21 17:36:50 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,15 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/notesdb";
+export const database = {
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+};
+
+export const port = process.env.PORT || 4000;
+
+export const SECRET = process.env.SECRET;
