a09a1030bfd0473c8b3428b4fe5d7ae51a620f2e
Bobhate Vishal <bobhatev13@gmail.com>
2025-08-21 18:08:27 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,13 +3,12 @@ config();
 
 export const database = {
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
 };
 
 export const port = process.env.PORT || 4000;
-
-export const SECRET = process.env.SECRET;
+export const SECRET = process.env.SECRET || "some secret key";
